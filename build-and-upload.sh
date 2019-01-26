14a9b97b3df4a46a32a21828bb485c48f5312c6e
Alex Williams <lexw@live.co.uk>
2019-01-26 21:52:12 +0000

    Got build script to work

--- build-and-upload.sh
+++ build-and-upload.sh
@@ -4,11 +4,9 @@ set +ex
 
 function buildAndPush {
     local version=$1
-    docker build -t alexswilliams/:${version} --build-arg VERSION=${version} --file Dockerfile.arm32v6 .
-    docker push
+    docker build -t alexswilliams/arm32v6-prometheus:${version} --build-arg VERSION=${version} --file Dockerfile.arm32v6 .
+    docker push alexswilliams/arm32v6-prometheus:${version}
 }
-
-docker login --username=alexswilliams
 
 buildAndPush "2.6.1"
 
