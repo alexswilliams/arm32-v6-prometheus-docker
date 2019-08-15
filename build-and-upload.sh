f8c5c4aeff245cbb89219b45ee06be0947fa40b7
Alex Williams <alex.williams@skybettingandgaming.com>
2019-08-15 23:40:39 +0100

    Renamed dockerfile and pushed latest versions

--- build-and-upload.sh
+++ build-and-upload.sh
@@ -5,13 +5,13 @@ set -ex
 function buildAndPush {
     local version=$1
     local imagename="alexswilliams/arm32v6-prometheus"
-    local fromline=$(grep -e '^FROM ' Dockerfile.arm32v6 | tail -n -1 | sed 's/^FROM[ \t]*//' | sed 's#.*/##' | sed 's/:/-/' | sed 's/#.*//' | sed -E 's/[ \t]+.*//')
+    local fromline=$(grep -e '^FROM ' Dockerfile | tail -n -1 | sed 's/^FROM[ \t]*//' | sed 's#.*/##' | sed 's/:/-/' | sed 's/#.*//' | sed -E 's/ +.*//')
 
     docker build -t ${imagename}:${version} \
         --build-arg VERSION=${version} \
         --build-arg BUILD_DATE=$(date -u +"%Y-%m-%dT%H:%M:%SZ") \
         --build-arg VCS_REF=$(git rev-parse --short HEAD) \
-        --file Dockerfile.arm32v6 . \
+        --file Dockerfile . \
     && docker tag ${imagename}:${version} ${imagename}:${version}-${fromline} \
     && docker push ${imagename}:${version} \
     && docker push ${imagename}:${version}-${fromline}
@@ -27,7 +27,8 @@ function buildAndPush {
 #buildAndPush "2.9.1"
 #buildAndPush "2.9.2"
 #buildAndPush "2.10.0"
-buildAndPush "2.11.0"
+#buildAndPush "2.11.0"
 buildAndPush "2.11.1"
+buildAndPush "2.11.2"
 
 curl -X POST "https://hooks.microbadger.com/images/alexswilliams/arm32v6-prometheus/H8lh7yTJah4vJT69Kjz-00QLM44="
