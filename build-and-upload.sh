e54afb55232953bdb367a1086fbb5a638db96346
Alex Williams <alex.williams@skybettingandgaming.com>
2020-06-03 21:56:08 +0100

    added missing push instruction

--- build-and-upload.sh
+++ build-and-upload.sh
@@ -2,14 +2,16 @@
 
 set -ex
 
+export DOCKER_BUILDKIT=1
+
 function buildAndPush {
     local version=$1
     local imagename="alexswilliams/arm32v6-prometheus"
     local fromline=$(grep -e '^FROM ' Dockerfile | tail -n -1 | sed 's/^FROM[ \t]*//' | sed 's#.*/##' | sed 's/:/-/' | sed 's/#.*//' | sed -E 's/ +.*//')
     local latest="last-build"
     if [ "$2" == "latest" ]; then latest="latest"; fi
-
-    docker buildx build \
+    
+    docker build \
         --platform=linux/arm/v6 \
         --build-arg VERSION=${version} \
         --build-arg BUILD_DATE=$(date -u +"%Y-%m-%dT%H:%M:%SZ") \
@@ -17,8 +19,12 @@ function buildAndPush {
         --tag ${imagename}:${version} \
         --tag ${imagename}:${version}-${fromline} \
         --tag ${imagename}:${latest} \
-        --file Dockerfile .
-#        --push \
+        --file Dockerfile \
+        .
+    
+    docker push ${imagename}:${version}
+    docker push ${imagename}:${version}-${fromline}
+    docker push ${imagename}:${latest}
 }
 
 #buildAndPush "2.7.0"
